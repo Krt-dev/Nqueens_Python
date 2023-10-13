aa7756f6ec1bd9e4d287cf4b814f3a882c9b9a9b
Kurt Sanchez <kurt.sanchez.20@usjr.edu.ph>
2023-10-13 12:11:07 +0800

    final iter

--- nqueen.py
+++ nqueen.py
@@ -1,50 +1,50 @@
-def is_safe(board, row, col, N):
-    # Check if there is a queen in the same column
+def is_safe(board, row, col, n):
+    # Check the column on top of the current cell
     for i in range(row):
-        if board[i][col] == "Q":
+        if board[i][col] == 1:
             return False
 
-    # Check upper left diagonal
+    # Check upper-left diagonal
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == "Q":
+        if board[i][j] == 1:
             return False
 
-    # Check upper right diagonal
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == "Q":
+    # Check upper-right diagonal
+    for i, j in zip(range(row, -1, -1), range(col, n)):
+        if board[i][j] == 1:
             return False
 
     return True
 
-def solve_n_queens(N):
-    board = [["." for _ in range(N)] for _ in range(N)]
+def solve_n_queens(n):
+    board = [[0 for _ in range(n)] for _ in range(n)]
 
-    def backtrack(row):
-        if row == N:
-            solutions.append(["".join(row) for row in board])
-            return
-        for col in range(N):
-            if is_safe(board, row, col, N):
-                board[row][col] = "Q"
-                backtrack(row + 1)
-                board[row][col] = "."
+    if solve_n_queens_util(board, 0, n):
+        return board
 
-    solutions = []
-    backtrack(0)
-    return solutions
+    return None
 
-def display_solution(solution):
-    for row in solution:
-        print("[" + "][".join(row) + "]")
-    print()
+def solve_n_queens_util(board, row, n):
+    if row == n:
+        return True
+
+    for col in range(n):
+        if is_safe(board, row, col, n):
+            board[row][col] = 1
+            if solve_n_queens_util(board, row + 1, n):
+                return True
+            board[row][col] = 0
+
+    return False
+
+def display_board(board):
+    if board is None:
+        print("No solution exists.")
+    else:
+        for row in board:
+            print(' '.join(['[Q]' if cell == 1 else '[.]' for cell in row]))
 
 if __name__ == "__main__":
-    N = int(input("Enter the board size (N): "))
-    solutions = solve_n_queens(N)
-    
-    if solutions:
-        print(f"Found {len(solutions)} solution(s) for {N}-Queens:")
-        for solution in solutions:
-            display_solution(solution)
-    else:
-        print(f"No solutions found for {N}-Queens.")
+    n = int(input("Enter the board size (wN): "))
+    solution = solve_n_queens(n)
+    display_board(solution)
